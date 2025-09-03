310d1697b7a0b256ecf1cf2afc03382474871de3
Shane Donaghy <donaghys@tcd.ie>
2025-09-03 23:21:52 +0100

    i18n implemented - english and ukrainian on the logout button alone

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default [
+  {
+    env: {
+      browser: true,
+      es2021: true
+    },
+    extends: [
+      "plugin:svelte/recommended",
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "prettier",
+      "plugin:prettier/recommended"
+    ],
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: "module",
+      project: "./tsconfig.json",
+      extraFileExtensions: [".svelte"]
+    },
+    overrides: [
+      {
+        files: ["*.svelte"],
+        processor: "svelte/svelte",
+        parser: "svelte-eslint-parser",
+        parserOptions: {
+          parser: "@typescript-eslint/parser"
+        }
+      }
+    ],
+    rules: {
+      "no-inner-declarations": "off",
+      "prettier/prettier": "error"
+    }
+  }
+];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './app.css'
 import App from './App.svelte'
 import {mount} from "svelte";
+import './i18n';
 
 const app = mount(App, {target: document.getElementById("app")});
 
